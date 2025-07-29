6b5777c3a42dbb4761b551a24f3f8902d210cb4d
VLSI II lecture <vlsi2_12fs25@tardis-a07.ee.ethz.ch>
2025-07-30 00:40:17 +0200

    final result

---
+++ calibre/lvs/v2lvs.tcl
@@ -0,0 +1,14 @@
+v2lvs::set_warning_level -level 4
+#v2lvs::set_warning_level -level 1
+v2lvs::override_globals -default_not_connected
+# needed that the bus connections are correct and
+# the spice.inc is also taken in account, it is needed for the VNW VPW connection
+v2lvs::combine_interface_info  -enable
+v2lvs::load_verilog -filename ../../openroad/out/croc_lvs.v
+v2lvs::load_verilog -filename ./verilog.inc -lib_mode
+v2lvs::load_spice   -filename ./spice.inc -range_mode
+v2lvs::set_includes -filename ./spice.inc
+v2lvs::override_globals -supply0 VSS
+v2lvs::override_globals -supply1 VDD
+v2lvs::write_output -filename croc_chip.spice
+exit
